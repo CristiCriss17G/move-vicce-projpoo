3e829547821ec54fdd8619cf8b8f2b59708d6c61
Cristian Iordachescu <iordachescu1996@outlook.com>
2022-12-02 16:58:54 +0200

    am adaugat o linie

--- src/main/java/com/vicce/move/App.java
+++ src/main/java/com/vicce/move/App.java
@@ -4,10 +4,10 @@ package com.vicce.move;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("Hello Cristi!");
     }
 }
