dfba788ee0180b8fb22e7bc002c99b8dd84b67db
Hodina Emilia <emilia.cristiana@yahoo.com>
2022-12-14 19:02:49 +0200

    am adaugat vectorul

--- src/main/java/com/vicce/move/App.java
+++ src/main/java/com/vicce/move/App.java
@@ -1,15 +1,50 @@
 package com.vicce.move;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
     public static void main(String[] args) {
-        VehiculCuMotor vcm = new VehiculCuMotor(100, 1000, 4, 5);
 
-        vcm.afisare();
+        VehiculMBenzina v1= new VehiculMBenzina(200,7000,4,5,250,1600,"VW","Golf 5",2015,9.8);
+        VehiculMBenzina v2= new VehiculMBenzina(250,8000,4,5,143,1968,"Audi","A3",2008,5.6);
+        VehiculMBenzina v3= new VehiculMBenzina(180,6000,4,5,150,1595,"Ford","Focus", 2012,5.6);
+        VehiculMBenzina v4= new VehiculMBenzina();
+        VehiculMBenzina v5= new VehiculMBenzina(220,12000,4,5,170,1995,"BMW","Seria 3",2014,7.1);
+        VehiculMBenzina v6= new VehiculMBenzina(v2);
+        VehiculMBenzina v7= new VehiculMBenzina(200,15400,4,5,123,1598,"Hyundai","Elantra",2019,7.2);
+        VehiculMBenzina v8= new VehiculMBenzina(170,4200,4,5,85,1400,"Citroen","C3",2007,6.3);
+        VehiculMBenzina v9= new VehiculMBenzina(v5);
+        VehiculMBenzina v10= new VehiculMBenzina(220,11350,4,5,160,1798,"Skoda","Superb",2014,10.4);
 
-        System.out.println(vcm.raportVitezaPret());
+
+        ArrayList<Object> masini =new ArrayList<Object>(10);
+        masini.add(v1);
+        masini.add(v2);
+        masini.add(v3);
+        masini.add(v4);
+        masini.add(v5);
+        masini.add(v6);
+        masini.add(v7);
+        masini.add(v8);
+        masini.add(v9);
+        masini.add(v10);
+
+        //Afisare elemente din vector (forEach loop)
+
+        for(Object i:masini){
+            System.out.println(i.toString());
+        }
+        
+        //Afisare elemente din vector cu Iterator
+
+        Iterator<Object> itr=masini.iterator();
+
+        while(itr.hasNext()){
+            Object obj=itr.next();
+            System.out.println(obj.toString());
+        }
+
+        
     }
 }
--- src/main/java/com/vicce/move/VehiculMBenzina.java
+++ src/main/java/com/vicce/move/VehiculMBenzina.java
@@ -1,96 +1,102 @@
 package com.vicce.move;
 
 public class VehiculMBenzina extends VehiculCuMotor {
-    protected int caiPutere;
-    protected int capacitateMotor;
-    protected String marca;
-    protected String model;
-    protected int anFabricatie;
-    protected double consumUrban;
+    int caiPutere;
+    int capacitateMotor;
+    String marca;
+    String model;
+    int anFabricatie;
+    double consumUrban;
 
-    VehiculMBenzina() {
+
+    VehiculMBenzina(){
         super();
-        this.caiPutere = 0;
-        this.capacitateMotor = 0;
-        this.marca = "";
-        this.model = "";
-        this.anFabricatie = 2000;
-        this.consumUrban = 0;
+        caiPutere=100;
+        capacitateMotor=1600;
+        marca="VW";
+        model="Passat";
+        anFabricatie=2000;
+        consumUrban=5.6;
     }
 
-    VehiculMBenzina(float vitezaMax, float pret, int nrRoti, int nrLocuri, int putere, int capacitate, String marca,
-            String model, int an, double consum) {
-        super(vitezaMax, pret, nrRoti, nrLocuri);
-        this.caiPutere = putere;
-        this.capacitateMotor = capacitate;
-        this.marca = marca;
-        this.model = model;
-        this.anFabricatie = an;
-        this.consumUrban = consum;
+    VehiculMBenzina(float vitezaMax, float pret, int nrRoti, int nrLocuri,int putere,int capacitate,String marca,String model,int an, double consum){
+        super(vitezaMax,pret,nrRoti,nrLocuri);
+        this.caiPutere=putere;
+        this.capacitateMotor=capacitate;
+        this.marca=marca;
+        this.model=model;
+        this.anFabricatie=an;
+        this.consumUrban=consum;
     }
 
-    VehiculMBenzina(VehiculMBenzina m) {
+    VehiculMBenzina(VehiculMBenzina m){
         super(m);
-        this.caiPutere = m.caiPutere;
-        this.capacitateMotor = m.capacitateMotor;
-        this.marca = m.marca;
-        this.model = m.model;
-        this.anFabricatie = m.anFabricatie;
-        this.consumUrban = m.consumUrban;
+        this.caiPutere=m.caiPutere;
+        this.capacitateMotor=m.capacitateMotor;
+        this.marca=m.marca;
+        this.model=m.model;
+        this.anFabricatie=m.anFabricatie;
+        this.consumUrban=m.consumUrban;
     }
 
-    public int getCaiPutere() {
-        return this.caiPutere;
+    public int getCaiPutere(){
+        return caiPutere;
     }
 
-    public void setCaiPutere(int putere) {
-        this.caiPutere = putere;
+    public void setCaiPutere(int putere){
+        this.caiPutere=putere;
     }
 
-    public int getCapacitateMotor() {
-        return this.capacitateMotor;
+    public int getCapacitateMotor(){
+        return capacitateMotor;
     }
 
-    public void setCapacitateMotor(int capacitate) {
-        this.capacitateMotor = capacitate;
+    public void setCapacitateMotor(int capacitate){
+        this.capacitateMotor=capacitate;
     }
 
-    public String getMarca() {
-        return this.marca;
+    public String getMarca(){
+        return marca;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
+    public void setMarca(String marca){
+        this.marca=marca;
     }
 
-    public String getModel() {
-        return this.model;
+    public String getModel(){
+        return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    public void setModel(String model){
+        this.model=model;
     }
 
-    public int getAnFabricatie() {
-        return this.anFabricatie;
+    public int getAnFabricatie(){
+        return anFabricatie;
+    }
+    
+    public void setAnFabricatie(int an){
+        this.anFabricatie=an;
     }
 
-    public void setAnFabricatie(int an) {
-        this.anFabricatie = an;
+    public double getConsumUrban(){
+        return consumUrban;
     }
-
-    public double getConsumUrban() {
-        return this.consumUrban;
-    }
-
-    public void setConsumUrban(double consum) {
-        this.consumUrban = consum;
+    
+    public void setConsumUrban(double consum){
+        this.consumUrban=consum;
     }
 
     @Override
-    public void afisare() {
-        System.out.println("Vehicul cu motor pe benzina " + marca + " " + model + " fabricat in anul " + anFabricatie
-                + " cu " + caiPutere + "cai putere" + " si o capacitate de " + capacitateMotor
-                + " si un consum urban de " + consumUrban + "l/100km");
+    public String toString(){
+        return "Vehicul cu motor pe benzina "+marca+" "+ model+" fabricat in anul "+anFabricatie+" cu "+caiPutere+" cai putere"+" si o capacitate de "+capacitateMotor+ " si un consum urban de "+consumUrban+"l/100km";
+
     }
+    
+
+    @Override
+    public void afisare(){
+        System.out.println("Vehicul cu motor pe benzina "+marca+" "+ model+" fabricat in anul "+anFabricatie+" cu "+caiPutere+"cai putere"+" si o capacitate de "+capacitateMotor+ " si un consum urban de "+consumUrban+"l/100km");
+    }
+
 }
