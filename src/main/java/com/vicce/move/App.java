9bea32f5b7c3938c7d86bc974149cd8b2e426f78
gorobtovvladandrei <gorobtov.vlad@ivfa.ro>
2022-12-15 13:36:49 +0200

    Modificare App

--- src/main/java/com/vicce/move/App.java
+++ src/main/java/com/vicce/move/App.java
@@ -1,15 +1,62 @@
 package com.vicce.move;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import com.vicce.move.seeder.VehiculFMAgrementSeeder;
+
 /**
- * Hello world!
+ * Testeaza clasa VehiculFMAgrement
  *
  */
 public class App {
     public static void main(String[] args) {
-        VehiculCuMotor vcm = new VehiculCuMotor(100, 1000, 4, 5);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Optiuni: ");
+        System.out.println("1. Vizualizare vehicule din baza de date");
+        System.out.println("2. Adaugare vehicule in baza de date");
+        System.out.println("3. Stergere vehicule din baza de date si refacere baza de date");
 
-        vcm.afisare();
+        System.out.println("Introduceti numarul optiunii: ");
+        int optiune = scanner.nextInt();
+        switch (optiune) {
+            case 1:
+                try {
+                    ArrayList<VehiculFMAgrement> vehicule2 = VehiculFMAgrementSeeder.JSONReadSeed("vehicule.test.json");
+                    for (VehiculFMAgrement vehicul : vehicule2) {
+                        vehicul.afisare();
+                        System.out.println(vehicul.raportVitezaPret());
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Nu exista vehicule in baza de date");
+                }
+                break;
+            case 2:
+                System.out.println("Introduceti numarul de vehicule de tipul VehiculFMAgrement: ");
+                int nr = scanner.nextInt();
+                ArrayList<VehiculFMAgrement> vehiculeOld;
+                try {
+                    vehiculeOld = VehiculFMAgrementSeeder.JSONReadSeed("vehicule.test.json");
+                } catch (IllegalArgumentException e) {
+                    vehiculeOld = new ArrayList<VehiculFMAgrement>();
+                }
+                ArrayList<VehiculFMAgrement> vehiculeNew = VehiculFMAgrementSeeder.seed(nr);
+                ArrayList<VehiculFMAgrement> vehicule = new ArrayList<VehiculFMAgrement>();
+                vehicule.addAll(vehiculeOld);
+                vehicule.addAll(vehiculeNew);
+                VehiculFMAgrementSeeder.JSONseed(vehicule);
+                break;
+            case 3:
+                System.out.println("Introduceti numarul de vehicule de tipul VehiculFMAgrement: ");
+                nr = scanner.nextInt();
+                ArrayList<VehiculFMAgrement> vehicule3 = VehiculFMAgrementSeeder.seed(nr);
+                VehiculFMAgrementSeeder.JSONseed(vehicule3);
+                break;
+            default:
+                System.out.println("Optiune invalida");
+                break;
+        }
 
-        System.out.println(vcm.raportVitezaPret());
+        scanner.close();
     }
 }
