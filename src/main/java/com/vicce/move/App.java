77c988594d9c77e46d636dea490b72a0f331bcfa
cristianiivfa2022 <iordachescu.cristian@ivfa.ro>
2023-01-11 11:59:31 +0200

    integrated javafx

--- src/main/java/com/vicce/move/App.java
+++ src/main/java/com/vicce/move/App.java
@@ -1,15 +1,51 @@
 package com.vicce.move;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
- * Hello world!
+ * Base class for the app with javaFX
  *
  */
-public class App {
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
     public static void main(String[] args) {
         VehiculCuMotor vcm = new VehiculCuMotor(100, 1000, 4, 5);
 
         vcm.afisare();
 
         System.out.println(vcm.raportVitezaPret());
+
+        // try {
+        // System.out.println(loadFXML("primary"));
+        // } catch (IOException e) {
+        // System.out.println("IOException");
+        // System.out.println(e);
+        // }
+
+        launch();
     }
 }
