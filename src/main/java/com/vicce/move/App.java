3f45577f8673099d8470d012656239ffc450ed06
Gorobtov <gorobtov.vlad@ivfa.ro>
2023-01-18 22:42:22 +0200

    DuringRebase

--- src/main/java/com/vicce/move/App.java
+++ src/main/java/com/vicce/move/App.java
@@ -33,12 +33,6 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        VehiculCuMotor vcm = new VehiculCuMotor(100, 1000, 4, 5);
-
-        vcm.afisare();
-
-        System.out.println(vcm.raportVitezaPret());
-
         launch();
     }
 }
