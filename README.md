6e8b9cf327973d9bb98772e5eb619b40ef1ab984
Zaharia Florina <zaharia.florina95@yahoo.com>
2023-02-03 13:22:00 +0200

    prezentare

---
+++ README.md
@@ -0,0 +1,28 @@
+## DESCRIERE
+Am realizat acest proiect la materia Programare Orientata pe Obiect, anul 2022-20223 in semestrul 1.
+
+In acest proiect am realizat o aplicatie care imi permite sa gestionez o baza de date cu automobile cu motor si fara motor. Aceste doua clase avand alte sublclase: 
+- pentru clasa cu motor avem subclasele, cu benzina, cu motrina si electric.
+- pentru clasa fara motor avem subclasele: sportiv si de agrement.
+
+Eu am implementat clasa vehicul cu motorina, iar scopul acestei aplicatii este de a-mi arata datele specifice despre acest vehicul. Pot modifica date, sa adug sau sa sterg, in functie de preferinte, avand si implementata functia functia pentru importare date dintr-un fisier.
+Am creat si un seeder apelat in interfata care imi permite sa generez date aleatorii utilizate intr-o aplicatie. Cu ajutorul acestui seeder pot testa aceasta aplicatie.
+
+Ideea finala a proiectului este generarea raportului pret-viteza pentru masinile cu motorina, adica mai exact rentabilitatea, astfel in interfata avem doua filtre, pret si viteza cu doua imputuri, minim si maxim, care imi afiseaza rezultatul in functie de cerinta.
+
+## Interfata Grafica
+Interfata a fost creata cu ajutorul JavaFX
+
+## Fisiere 
+- src/main/java/: conține fișierele sursă Java cu codul aplicației cat si seederul pentru a genera date aleatorii
+- src/main/resources/: conține fișierele ce tin de partea de interfata grafica a aplicatiei (fisiere .fxml, .css)
+
+
+## Instalare
+Pentru a rula aplicația, este necesară instalarea Maven și a Java JDK 19.
+
+## Rulare
+Pentru a rula aplicația, se rulează comanda Play din VS Code.
+
+## Pentru mai multe detalii
+Pentru mai multe detalii despre proiect: inna_srb@yahoo.com
--- src/main/java/com/vicce/move/AddVehiculController.java
+++ src/main/java/com/vicce/move/AddVehiculController.java
@@ -1,11 +1,21 @@
 package com.vicce.move;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.stage.FileChooser;
 import javafx.util.converter.IntegerStringConverter;
+
+import java.util.ArrayList;
 import java.util.function.UnaryOperator;
 
 import com.vicce.move.seeder.*;
@@ -13,6 +23,11 @@ import com.vicce.move.seeder.*;
 public class AddVehiculController {
     @FXML
     private TextField textFieldAddVehicule;
+
+    @FXML
+    private TextField textFieldFilePath;
+
+    private String filePath;
 
     @FXML
     private Label labelStatus;
@@ -50,5 +65,59 @@ public class AddVehiculController {
             labelStatus.setText("Vehicule adaugate cu succes!");
         else
             labelStatus.setText("Nu s-au putut adauga vehiculele!");
+    }
+
+    @FXML
+    private void importData() {
+        if (filePath == null || filePath.isEmpty()) {
+            labelStatus.setText("Nu ati selectat un fisier!");
+            return;
+        }
+        try {
+            // open file and print contents
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                // split line by \/ and based on the first element of the array as the class
+                // name, call the appropriate constructor
+                // and add the object to the list
+                String[] lineArray = line.split(";");
+                Mobilitate mobilitate = null;
+                switch (lineArray[0]) {
+
+                    case "VehiculMMotorina":
+                        long id = Long.parseLong(lineArray[1]);
+                        float vitezaMax = Float.parseFloat(lineArray[2]);
+                        float pret = Float.parseFloat(lineArray[3]);
+                        String marca = lineArray[4];
+                        String model = lineArray[5];
+                        String tip = lineArray[6];
+                        String proprietar = "";
+                        int stock = Integer.parseInt(lineArray[7]);
+                        int nrRoti = Integer.parseInt(lineArray[8]);
+                        int nrLocuri = Integer.parseInt(lineArray[9]);
+                        int nrCilindri = Integer.parseInt(lineArray[10]);
+                        int an = Integer.parseInt(lineArray[11]);
+                        int putere = Integer.parseInt(lineArray[12]);
+                        float cupluMot = Float.parseFloat(lineArray[13]);
+
+                        mobilitate = new VehiculMMotorina(vitezaMax, pret, marca, model, tip, proprietar, stock, nrRoti,
+                                nrLocuri, nrCilindri, an, putere, cupluMot, id);
+                        break;
+                    default:
+                        System.out.println("Invalid class name");
+                }
+                if (mobilitate != null)
+                    Seeder.addVehicule(mobilitate);
+            }
+            bufferedReader.close();
+            App.setRoot("primaryFMAgrement");
+        } catch (FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + filePath + "'");
+        } catch (IOException ex) {
+            System.out.println("Error reading file '" + filePath + "'");
+        }
     }
 }
